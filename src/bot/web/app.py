fcc535efdd6558b5dc4b103debb2ecf4c1e44c26
Rizen <123223112+royalrizen@users.noreply.github.com>
2026-08-29 19:09:08 +0530

    Update app.py

--- src/bot/web/app.py
+++ src/bot/web/app.py
@@ -2,18 +2,9 @@ import os
 from threading import Thread
 from flask import Flask, render_template
 
-BASE_DIR = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "../../../..")
-)
 
 app = Flask(
-    __name__,
-    template_folder=os.path.join(
-        BASE_DIR,
-        "templates",
-    ),
-)
-
+    __name__)
 
 @app.route("/")
 def index():
