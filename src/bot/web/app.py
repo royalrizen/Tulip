7a70c4d063c9ed385477bbdfbdcfcc84da647963
Rizen <123223112+royalrizen@users.noreply.github.com>
2026-08-29 18:59:36 +0530

    Update app.py

--- src/bot/web/app.py
+++ src/bot/web/app.py
@@ -2,21 +2,21 @@ import os
 from threading import Thread
 from flask import Flask, render_template
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-TEMPLATE_DIR = os.path.join(BASE_DIR, "web", "templates")
-
 app = Flask(
     __name__,
-    template_folder=TEMPLATE_DIR,
+    template_folder="templates",
 )
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.get("/health")
 def health():
     return {"status": "ok"}, 200
+
 
 def run_web():
     port = int(os.getenv("PORT", 8080))
@@ -25,6 +25,7 @@ def run_web():
         host="0.0.0.0",
         port=port,
     )
+
 
 def start_web():
     Thread(
